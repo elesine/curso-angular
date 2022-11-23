2cf6e380e2f5da0c86e21348ad4c255c42057144
Gretel Danitza Canasa Condori <gcanasac@bsg.com>
2022-11-22 19:01:57 -0500

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Importar Componentes
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { FrutaComponent } from './fruta/fruta.component';
+
+const routes: Routes = [
+  {path: 'empleado', component: EmpleadoComponent},
+  {path: 'fruta', component: FrutaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
